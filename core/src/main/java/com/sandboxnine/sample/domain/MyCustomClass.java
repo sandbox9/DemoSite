523bea6a395f7665ef88f5488bafeb864cf06750
Dennis,Koo <gwkoo@itwise.co.kr>
2014-12-02 21:19:33 +0900

    Admin Module Sample 따라 해본거

---
+++ core/src/main/java/com/sandboxnine/sample/domain/MyCustomClass.java
@@ -0,0 +1,15 @@
+package com.sandboxnine.sample.domain;
+
+import java.io.Serializable;
+
+public interface MyCustomClass extends Serializable {
+
+	Long getId();
+
+	void setId(Long id);
+
+	String getName();
+
+	void setName(String name);
+
+}
